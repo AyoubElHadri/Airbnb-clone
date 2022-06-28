e00c0219c14d4697aec95dc12e238ad771b643ca
Ayoubelhadri <ayoubelhadri@outlook.com>
2022-06-28 19:31:58 +0200

    last commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,44 @@
 import React from 'react'
 import Image1 from '../images/image1.png'
+import Image2 from '../images/image2.png'
+import Image3 from '../images/image3.png'
 import Star from '../images/Star.png'
 const Card = () => {
   return (
-    <div className='card'>
+    <div className='cards' >
+      <div className='card'>
         <img src={Image1} className='card-image' />
         <div className='card-stats'>
-            <img src={Star} />
-            <span>5.0</span>
-            <span>(6) </span>
-            <span>USA</span>
-            <p>Life Lessons with Katie</p>
-            <p>From $136 / person</p>
+            <img src={Star} className='star'/>
+            <span>4.0</span>
+            <span className='text-gray'>(4) </span>
+            <span className='text-gray'>USA</span></div>
+            <p className='text-detail'>Life Lessons with Zaferes</p>
+            <p className='text-detail' > <span className='bold'>From $136</span> / person</p>
 
-        </div>
+            </div>
+      <div className='card'>
+        <img src={Image2} className='card-image' />
+        <div className='card-stats'>
+            <img src={Star} className='star'/>
+            <span>4.5</span>
+            <span className='text-gray'>(3) </span>
+            <span className='text-gray'>USA</span></div>
+            <p className='text-detail'>Life Lessons with Marie</p>
+            <p className='text-detail' > <span className='bold'>From $70</span> / person</p>
+
+            </div>
+      <div className='card'>
+        <img src={Image3} className='card-image' />
+        <div className='card-stats'>
+            <img src={Star} className='star'/>
+            <span>5.0</span>
+            <span className='text-gray'>(6) </span>
+            <span className='text-gray'>Canada</span></div>
+            <p className='text-detail'>Life Lessons with Leo</p>
+            <p className='text-detail' > <span className='bold'>From $99</span> / person</p>
+
+            </div>
     </div>
   )
 }
